110842d1d34984e11a629a8cad6c5b7028aa7927
매코 <53283147+seokjw0727@users.noreply.github.com>
2023-07-29 22:43:58 +0900

    서버가 index.html 파일을 호스팅하도록 위치 변경

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-// Serve static files
+
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Route for the root URL
+
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'informaion.html'));
+  res.sendFile(path.join(__dirname, 'index.html')); 
 });
 
-// Start the server
+
 app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+  console.log('Server is running on port 3000');z
 });
